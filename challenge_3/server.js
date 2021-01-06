e379d07228f1103fe05e11caed868a5a14c094be
Adam Brent <adam.brent@gmail.com>
2021-01-06 12:18:27 -0800

    Adds bare bones server, updates index.html to include app.

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
+var express = require('express');
+var app = express();
+var port = 3002;
 
+app.use(express.static('public'));
 
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
