2d762df355c8e937625de1fed6c8c0c8935099d3
Adam Brent <adam.brent@gmail.com>
2021-01-07 15:28:54 -0800

    Adds placePiece function.

--- challenge_4/connectFour.js
+++ challenge_4/connectFour.js
@@ -1,3 +1,11 @@
+var game = {
+  board: buildBoard();
+  players: ['red', 'black'];
+  turnNumber: 1;
+  turn: players[turnNumber % 2];
+  gameOver: false;
+}
+
 var buildBoard = (rows = 6, columns = 7) => {
   let board = [];
   for (let row = 0; row < rows; row++) {
@@ -10,3 +18,25 @@ var buildBoard = (rows = 6, columns = 7) => {
   return board;
 };
 
+updateTurn = () => {
+  game.turnNumber++;
+  game.turn = players[turnNumber % 2];
+};
+
+var placePiece = (column) => {
+  for (row of board) {
+    for (let square = board.length; square >= 0; square--) {
+      if (!row[square]) {
+        row[square] = turn;
+        game.gameOver = endCheck();
+        if (!game.gameOver) {
+          updateTurn();
+          return `'It's now ${turn}'s turn.`;
+        } else {
+          return game.gameOver;
+        }
+      }
+    }
+  }
+  return `Error: Illegal move, column is full. Try again, ${turn}.`;
+};
