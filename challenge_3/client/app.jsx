e6428695482363281cc80e9c831436577eec57db
Adam Brent <adam.brent@gmail.com>
2021-01-07 09:29:26 -0800

    Starts adding React components.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Form F1 fields={'name', 'email', 'password'}/>
+        <Form F2 fields={'line1', 'line2', 'city', 'state', 'zipcode', 'phone'}/>
+        <Form F3 fields={'ccNumber', 'expirationDate', 'cvv', 'billingZipcode'}/>
+      </div>
+    );
+  }
+}
+
+var Button = (props) => {
+
+};
+
+var Field = (props) => {
+
+};
 
 
 ReactDOM.render(
