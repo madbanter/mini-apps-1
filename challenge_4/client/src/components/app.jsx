8b7d7d87a800b13b73dbe495aba34995e4a31fa3
Adam Brent <adam.brent@gmail.com>
2021-01-07 12:21:37 -0800

    Additional components.

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import Board from 'Board.jsx';
+import Banner from 'Banner.jsx';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      board: [],
+      message: ''
+    };
   }
 
   render() {
     return (
       <div>
-        <Board />
+        <Banner message={this.state.message}/>
+        <Board board={this.state.board}/>
       </div>
     );
   }
