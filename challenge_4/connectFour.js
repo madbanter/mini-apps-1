2488db46748ed369e76ee9034cb315e7da0a8724
Adam Brent <adam.brent@gmail.com>
2021-01-07 17:20:07 -0800

    Fixes in placePiece references to game object.

--- challenge_4/connectFour.js
+++ challenge_4/connectFour.js
@@ -1,11 +1,3 @@
-var game = {
-  board: buildBoard(),
-  players: ['red', 'black'],
-  turnNumber: 1,
-  turn: players[turnNumber % 2],
-  gameOver: false
-}
-
 var buildBoard = (rows = 6, columns = 7) => {
   let board = [];
   for (let row = 0; row < rows; row++) {
@@ -18,25 +10,43 @@ var buildBoard = (rows = 6, columns = 7) => {
   return board;
 };
 
+var game = {
+  board: buildBoard(),
+  players: ['red', 'black'],
+  turnNumber: 1,
+  turn: 'black',
+  gameOver: false
+}
+
 updateTurn = () => {
   game.turnNumber++;
-  game.turn = players[turnNumber % 2];
+  game.turn = game.players[game.turnNumber % 2];
 };
 
 var placePiece = (column) => {
-  for (row of board) {
-    for (let square = board.length; square >= 0; square--) {
-      if (!row[square]) {
-        row[square] = turn;
-        game.gameOver = endCheck();
-        if (!game.gameOver) {
-          updateTurn();
-          return `'It's now ${turn}'s turn.`;
-        } else {
-          return game.gameOver;
-        }
+  for (let row = game.board.length - 1; row >= 0; row--) {
+    if (!game.board[row][column]) {
+      game.board[row][column] = game.turn;
+      game.gameOver = endCheck();
+      if (!game.gameOver) {
+        updateTurn();
+        return `'It's now ${game.turn}'s turn.`;
+      } else {
+        return game.gameOver;
       }
     }
   }
-  return `Error: Illegal move, column is full. Try again, ${turn}.`;
+  return `Error: Illegal move, column is full. Try again, ${game.turn}.`;
+};
+
+var endCheck = () => {
+
+};
+
+var winCheck = () => {
+
+};
+
+var tieCheck = () => {
+
 };
