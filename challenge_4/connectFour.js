8a8d0dcd1faef98595f7d33a4048bbd52016d13f
Adam Brent <adam.brent@gmail.com>
2021-01-07 18:37:55 -0800

    Adds winCheck logic (no diagonal yet, WIP).

--- challenge_4/connectFour.js
+++ challenge_4/connectFour.js
@@ -11,9 +11,10 @@ var buildBoard = (rows = 6, columns = 7) => {
 };
 
 var game = {
+  boardSize: 42,
   board: buildBoard(),
   players: ['red', 'black'],
-  turnNumber: 1,
+  turns: 1,
   turn: 'black',
   gameOver: false
 }
@@ -40,13 +41,52 @@ var placePiece = (column) => {
 };
 
 var endCheck = () => {
-
+  var tie = tieCheck();
+  if (tie) {
+    return tie;
+  } else {
+    return winCheck();
+  }
 };
 
 var winCheck = () => {
-
+  // Iterate over submatrix (max size 9) containing latest move (row, column)
+  // Check for four consecutive squares of color {turn}
+  // for (let checkRow = Math.max(row - 3, 0); checkRow < Math.min(row + 3, game.board.length); checkRow++) {
+  if (game.turns < 6) {  // Can be more specific based on who goes first...
+    return false;
+  }
+  let consecutive = 1;
+  if (row < game.board.length - 3) {
+    for (checkRow = row + 1; checkRow < row + 4; checkRow++) {
+      if (game.board[checkRow][column] === game.turn) {
+        consecutive++;
+      } else {
+        consecutive = 0;
+        break;
+      }
+    }
+    if (consecutive === 4) {
+      return `Four in a row! ${turn} wins!`;
+    }
+  } else {
+    for (let checkColumn = Math.max(column - 3, 0); checkColumn < Math.min(column + 3, game.board[0].length); checkColumn++) {  // Can be more specific about not checking if there are not enough remaining slots for a win
+      if (game.board[row][checkColumn] === game.turn) {
+        consecutive++;
+        if (consecutive === 4) {
+          return `Four in a row! ${turn} wins!`;
+        }
+      } else {
+        consecutive = 0;
+      }
+    }
+    // Check the diagonal!
+  }
+    // if found, turn wins, return `${turn} wins!` [highlight win??]
+  // Otherwise return false
+  return false;
 };
 
 var tieCheck = () => {
-
+  return game.turns === game.boardSize ? 'Tie game. Game over.' : false;
 };
